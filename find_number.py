6740bf25f29b391e9c6471531ad361515c42ff3d
David Monaghan <monaghan.david@gmail.com>
2023-05-08 17:05:18 -0700

    oops

--- find_number.py
+++ find_number.py
@@ -6,21 +6,21 @@ def find_number_spanning_files(search_number, files):
     search_between_buffer = ''
     index = 0
     for file in files:
-        if index > 1:
-            index = 0
-            search_between_buffer = ''
-        if index > 0:
-            found = search_between_buffer.find(search_number)
-            if found != -1:
-                return (file, found)
-        search_between_file += file
-        with open(file, "r") as f:
-            file_start = file[0:search_len]
-            file_end = file[-search_len:0]
-            search_between_buffer += file_start + file_end
-            index = index + 1
+      if index > 1:
+          index = 0
+          search_between_buffer = ''
+      if index > 0:
+          found = search_between_buffer.find(search_number)
+          if found != -1:
+              return (file, found)
+      search_between_file = search_between_file + file
+      with open(file, "r") as f:
+        file_start = file[0:search_len]
+        file_end = file[-search_len:0]
+        search_between_buffer += file_start + file_end
+        index = index + 1
 
-def find_in_files(search_number, files)
+def find_in_files(search_number, files):
     for file in files:
         with open(file, "r") as f:
             contents = f.read()
